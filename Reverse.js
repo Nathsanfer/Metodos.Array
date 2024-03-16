35618d2d003f4183c3735c17b8e92115f403af96
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-16 12:17:51 -0300

    Código finalizado

--- Reverse.js
+++ Reverse.js
@@ -1,3 +1,10 @@
+/* Utilizando o reverse
+    Esse é um método usado para inverter a ordem dos elementos em um array. 
+    Ele modifica o array original, invertendo os elementos e retornando uma 
+    referência para o próprio array invertido. 
+*/
+
+// Crio o array1 - Cores
 let cores = [
     'Azul', 'Amarelo',
     'Vermelho', 'Verde',
@@ -5,15 +12,26 @@ let cores = [
     'Roxo', 'Branco',
     'Preto', 'Marrom'
 ];
-console.log(cores);
+//console.log(cores); Realizado para verificação do array 
 
-console.log("------------ Exemplo 1 ------------"); 
+console.log("------------ Exemplo 1 ------------"); // Realizado somente para separar
 console.log("Nomes de algumas cores:");
 
+/* Método reverse 1 - cores
+
+    array1.reverse();
+
+*/
+// Troquei o "array1" pelo nome do meu array1 "cores"
 cores.reverse();
 
+// Exibe as cores em ordem invertida
 console.log(cores);
 
+console.log("------------ Exemplo 2 ------------"); // Realizado somente para separar
+console.log("Do Contemporâneo para Pré-história:");
+
+// Crio o array2 
 let periódosDaHistória = [
     'Pré-história',
     'Idade Antiga',
@@ -21,11 +39,15 @@ let periódosDaHistória = [
     'Idade Moderna',
     'Idade Contemporânea'
 ]
-console.log(periódosDaHistória);
+//console.log(periódosDaHistória); Realizado para verificação do array 
 
-console.log("------------ Exemplo 2 ------------"); 
-console.log("Do Contemporâneo para Pré-história:");
+/* Método reverse 2 - Períodos da História
 
+    array2.reverse();
+
+*/
+// Troquei o "array2" pelo nome do meu array2 "períodosDaHitória"
 periódosDaHistória.reverse();
 
+// Exibe os périodos em ordem invertida
 console.log(periódosDaHistória);
