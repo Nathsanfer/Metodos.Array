de69a2a2c1175d6b5d2017067e6206602d68c197
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-15 23:41:25 -0300

    Some/finalizado

--- Some.js
+++ Some.js
@@ -1,20 +1,46 @@
-let numerosTransferencia = [
-    {nome: 'Julio', depositado: 50},
-    {nome: 'Anna', depositado: 20},
-    {nome: 'Marcelo', depositado: 70}
-];
-console.log(numerosTransferencia);
+/* Utilizando o some
+    Esse é um método muito semelhante ao "every". A diferença é que o "every"
+    só retorna true se todos os elementos passarem no teste. O "some" retorna 
+    true se pelo menos um elemento do Array passar no teste.
+*/
 
-console.log("------------ Exemplo 1 ------------")
+// Crio o array - Transferência 
+let numerosTransferencia = [
+    { nome: 'Julio', depositado: 50 },
+    { nome: 'Anna', depositado: 20 },
+    { nome: 'Marcelo', depositado: 70 }
+];
+//console.log(numerosTransferencia); Realizado para verificação do array 
+
+console.log("------------ Exemplo 1 ------------") // Realizado somente para separar
 console.log("Algum elemento é igual a 50: ")
 
+/*
+
+    const newVariable = array.some(element => element = 50);
+
+*/
+// Troquei o "const" por let 
+// Troquei o "newVariable" pelo nome "numeroIgual"
+// Troquei o "array" pelo nome do meu array "numerosTransferencia"
 let numeroIgual = numerosTransferencia.some(element => element = 50);
 
+// Exibe a resposta 
 console.log(numeroIgual);
 
-console.log("------------ Exemplo 2 ------------")
+console.log("------------ Exemplo 2 ------------") // Realizado somente para separar
 console.log("Algum elemento é um número negativo: ")
 
+/*
+
+    const newVariable1 = array.some((user, index, array) => user.name < 10);
+
+*/
+// Troquei o "const" por let 
+// Troquei o "newVariable1" pelo nome "numerosMenorQueDez"
+// Troquei o "array" pelo nome do meu array "mnumerosTrasferencia"
+// Troquei o "name" por "depositado"
 let numerosMenorQueDez = numerosTransferencia.some((user, index, array) => user.depositado < 10);
 
+// Exibe a resposta 
 console.log(numerosMenorQueDez);
