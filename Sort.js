7df6f24090886529a467d1543dcec94b67950be8
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-16 13:24:44 -0300

    Código finalizado

--- Sort.js
+++ Sort.js
@@ -1,3 +1,9 @@
+/* Utilizando o sort 
+    Esse é um método utilizado para ordenar os elementos de um array. 
+    Por padrão, o "sort" ordena os elementos do array colocando em 
+    ordem alfabética ou numérica crescente.
+*/
+// Crio o array - Chamada de alunos 
 let chamada = [
     { numero: 4, nome: 'Daniel' },
     { numero: 2, nome: 'Bianca' },
@@ -5,26 +11,54 @@ let chamada = [
     { numero: 1, nome: 'Amanda' },
     { numero: 3, nome: 'Carlos' }
 ]
-console.log(chamada);
+//console.log(chamada); Realizado para verificação do array 
 
-console.log("------------ Exemplo 1 ------------");
+console.log("------------ Exemplo 1 ------------"); // Realizado somente para separar
 console.log("Chamada do 1 ao 5:");
 
+/* Método sort 2
+
+    function new(a, b) {
+        return a.credit - b.credit;
+    }
+
+    array.sort(new);
+
+*/
+// Troquei o "new" pelo nome "numerosChamada" 
+// Troquei o "credit" por "numero"
 function numerosChamada(a, b) {
     return a.numero - b.numero;
 }
 
+// Troquei o "array" pelo nome do meu array "chamada"
+// Troquei o "new" pelo nome "numerosChamada" 
 chamada.sort(numerosChamada);
 
+// Exibe a chamada em ordem Crescente
 console.log(chamada);
 
-console.log("------------ Exemplo 2 ------------");
+console.log("------------ Exemplo 2 ------------"); // Realizado somente para separar
 console.log("Chamada do 5 ao 1:");
 
+/* Método sort 2
+
+    function new1(a, b) {
+        return b.credit - a.credit;
+    }
+
+    array.sort(new1);
+
+*/
+// Troquei o "new" pelo nome "numerosChamada1" 
+// Troquei o "credit" por "numero"
 function numerosChamada1(a, b) {
     return b.numero - a.numero;
 }
 
+// Troquei o "array" pelo nome do meu array "chamada"
+// Troquei o "new" pelo nome "numerosChamada1" 
 chamada.sort(numerosChamada1);
 
+// Exibe a chamada em ordem Decrescente 
 console.log(chamada);
