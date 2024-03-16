b9fe3aea4990761502584e1d689269757cb572dc
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-16 11:52:51 -0300

    Reverse/somente código

---
+++ Reverse.js
@@ -0,0 +1,31 @@
+let cores = [
+    'Azul', 'Amarelo',
+    'Vermelho', 'Verde',
+    'Rosa', 'Laranja',
+    'Roxo', 'Branco',
+    'Preto', 'Marrom'
+];
+console.log(cores);
+
+console.log("------------ Exemplo 1 ------------"); 
+console.log("Nomes de algumas cores:");
+
+cores.reverse();
+
+console.log(cores);
+
+let periódosDaHistória = [
+    'Pré-história',
+    'Idade Antiga',
+    'Idade Média',
+    'Idade Moderna',
+    'Idade Contemporânea'
+]
+console.log(periódosDaHistória);
+
+console.log("------------ Exemplo 2 ------------"); 
+console.log("Do Contemporâneo para Pré-história:");
+
+periódosDaHistória.reverse();
+
+console.log(periódosDaHistória);
